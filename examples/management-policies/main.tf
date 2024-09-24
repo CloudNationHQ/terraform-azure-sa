17d5e7f216e5eab93720f6ee220a08a7214e0b91
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2024-09-24 12:26:26 +0200

    feat!: upgrade azurerm provder to v4 (#111)
    BREAKING CHANGE: * Version 4 of the azurerm provider includes breaking changes.
    
    Co-authored-by: dkool <dkool@dkools-MacBook-Pro.local>

--- examples/management-policies/main.tf
+++ examples/management-policies/main.tf
@@ -7,19 +7,19 @@ module "naming" {
 
 module "rg" {
   source  = "cloudnationhq/rg/azure"
-  version = "~> 0.1"
+  version = "~> 2.0"
 
   groups = {
     demo = {
-      name   = module.naming.resource_group.name
-      region = "westeurope"
+      name     = module.naming.resource_group.name
+      location = "westeurope"
     }
   }
 }
 
 module "storage" {
   source  = "cloudnationhq/sa/azure"
-  version = "~> 1.0"
+  version = "~> 2.0"
 
   storage = {
     name              = module.naming.storage_account.name_unique
--- examples/private-endpoint/main.tf
+++ examples/private-endpoint/main.tf
@@ -7,27 +7,27 @@ module "naming" {
 
 module "rg" {
   source  = "cloudnationhq/rg/azure"
-  version = "~> 0.1"
+  version = "~> 2.0"
 
   groups = {
     demo = {
-      name   = module.naming.resource_group.name
-      region = "northeurope"
+      name     = module.naming.resource_group.name
+      location = "northeurope"
     }
   }
 }
 
 module "network" {
   source  = "cloudnationhq/vnet/azure"
-  version = "~> 2.0"
+  version = "~> 4.0"
 
   naming = local.naming
 
   vnet = {
-    name          = module.naming.virtual_network.name
-    location      = module.rg.groups.demo.location
-    resourcegroup = module.rg.groups.demo.name
-    cidr          = ["10.19.0.0/16"]
+    name           = module.naming.virtual_network.name
+    location       = module.rg.groups.demo.location
+    resource_group = module.rg.groups.demo.name
+    cidr           = ["10.19.0.0/16"]
 
     subnets = {
       sn1 = {
@@ -40,7 +40,7 @@ module "network" {
 
 module "storage" {
   source  = "cloudnationhq/sa/azure"
-  version = "~> 1.0"
+  version = "~> 2.0"
 
   storage = {
     name           = module.naming.storage_account.name_unique
@@ -53,9 +53,9 @@ module "storage" {
 
 module "private_dns" {
   source  = "cloudnationhq/pdns/azure"
-  version = "~> 0.1"
+  version = "~> 1.0"
 
-  resourcegroup = module.rg.groups.demo.name
+  resource_group = module.rg.groups.demo.name
 
   zones = {
     blob = {
@@ -72,10 +72,10 @@ module "private_dns" {
 
 module "privatelink" {
   source  = "cloudnationhq/pe/azure"
-  version = "~> 0.1"
+  version = "~> 1.0"
 
-  resourcegroup = module.rg.groups.demo.name
-  location      = module.rg.groups.demo.location
+  resource_group = module.rg.groups.demo.name
+  location       = module.rg.groups.demo.location
 
   endpoints = {
     blob = {
