6d0cfba358867f60675bd51eb56769da0b4a989b
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2026-04-03 09:28:39 +0200

    feat: add missing outputs (#207)

--- outputs.tf
+++ outputs.tf
@@ -32,3 +32,23 @@ output "file_system_paths" {
   description = "file system paths configuration specifics"
   value       = azurerm_storage_data_lake_gen2_path.pa
 }
+
+output "management_policy" {
+  description = "management policy configuration specifics"
+  value       = azurerm_storage_management_policy.mgmt_policy
+}
+
+output "container_immutability_policies" {
+  description = "container immutability policy configuration specifics"
+  value       = azurerm_storage_container_immutability_policy.immutability_policy
+}
+
+output "local_users" {
+  description = "local user configuration specifics"
+  value       = azurerm_storage_account_local_user.lu
+}
+
+output "role_assignments" {
+  description = "role assignment configuration specifics"
+  value       = azurerm_role_assignment.managed_identity
+}
